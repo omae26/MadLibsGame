e2ecd1d02ee77f384cd111a0654962572dcfb33b
omae26 <omaekemunto26@gmail.com>
2025-04-19 22:32:30 +0300

    A simple mad libs game

---
+++ mads.py
@@ -0,0 +1,14 @@
+# 19/04/2025
+# This script is all about taking random words from a list and creating a story with them.
+
+# The story will be a random combination of the words in the list.
+# Users will be able to add their own words to the list
+# We will create a variable to store the users'inputs
+
+color = input("Enter a color: ")
+plural_noun =input("Enter a plural npoun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color )
+print(plural_noun + " are blue")
+print("I love " + celebrity)
