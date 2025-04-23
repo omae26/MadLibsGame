1a37ccd38d6b52aef938de7a13bd537ce9fa8537
omae26 <omaekemunto26@gmail.com>
2025-04-23 22:33:25 +0300

    I tried creating a mad libs game

---
+++ mad3.py
@@ -0,0 +1,11 @@
+print("Let's play Mad Libs")
+
+village = input("Enter a village: ")
+name = input("Enter a name: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+
+print("Once upon a time in a village called " + village + ", there lived a man named " + name + ". ")
+print("He lived in a small " + noun + ". " )
+print("He used to " + verb + " Children ")
+print("Everyone in the village loved him.") 
