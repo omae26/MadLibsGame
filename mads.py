238a066c49b374c30a4e19845c754869f9db1ee5
omae26 <162116251+omae26@users.noreply.github.com>
2025-04-28 23:49:49 +0300

    Update mads.py

--- mads.py
+++ mads.py
@@ -3,12 +3,14 @@
 
 # The story will be a random combination of the words in the list.
 # Users will be able to add their own words to the list
-# We will create a variable to store the users'inputs
+# We will create a variable to store users'inputs
 
 color = input("Enter a color: ")
-plural_noun =input("Enter a plural npoun: ")
+plural_noun =input("Enter a plural noun: ")
 celebrity = input("Enter a celebrity: ")
+noun = input("Enter a noun: ")
 
 print("Roses are " + color )
 print(plural_noun + " are blue")
 print("I love " + celebrity)
+print("I hate " + noun + ".")
