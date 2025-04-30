778321569be1ed225fb90ac447206884b73a78a8
omae26 <162116251+omae26@users.noreply.github.com>
2025-04-30 23:04:40 +0300

    definition

--- mads.py
+++ mads.py
@@ -1,5 +1,6 @@
 # 19/04/2025
-# This script is all about taking random words from a list and creating a story with them.
+# This script is about taking random words from a list and creating a story with them.
+# It is a mad libs game
 
 # The story will be a random combination of the words in the list.
 # Users will be able to add their own words to the list
